364e79a2a70e05478908db0deb70f32016798af2
Todd MC Tsai <toddmctsai@gmail.com>
2024-03-02 14:03:24 +0800

    add toggle for show/hide completed tasks

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -12,9 +12,15 @@
                     <Tasks :tasks="uncompletedTasks" />
                     
                     <!-- show toggle button -->
-
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary"
+                            @click="$event => showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show Completed</span>
+                            <span v-else>Hide Completed</span>
+                        </button>
+                    </div>
                     <!-- list of completed tasks -->
-                    <Tasks :tasks="completedTasks" />
+                    <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
 
                 </div>
             </div>
@@ -35,4 +41,11 @@ onMounted(async () => {
 
 const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
 const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+const showToggleCompletedBtn = computed(
+    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+)
+const completedTasksIsVisible = computed(
+    () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+)
+const showCompletedTasks = ref(false)
 </script>
