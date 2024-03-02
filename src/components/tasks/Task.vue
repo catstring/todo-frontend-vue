cb95153e2db8611a51acdb781e29a66ab53b375a
Todd MC Tsai <toddmctsai@gmail.com>
2024-03-02 17:28:54 +0800

    add edit update db

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="list-group-item py-3" >
+    <li class="list-group-item py-3">
         <div class="d-flex justify-content-start align-items-center">
             <input class="form-check-input mt-0" :class="completedClass" type="checkbox" :checked="task.is_completed" />
             <div class="ms-2 flex-grow-1" 
@@ -8,24 +8,29 @@
                 @dblclick="isEdit = true"
             >
                 <div class="relative" v-if="isEdit">
-                    <input class="editable-task" type="text" @keyup.esc="isEdit = false" v-focus />
+                    <input class="editable-task" 
+                        type="text" 
+                        @keyup.esc="isEdit = false" v-focus 
+                        @keyup.enter="updateTask"
+                    />
                 </div>
                 <span v-else>{{ task.name }}</span>
             </div>
             <!-- <div class="task-date">24 Feb 12:00</div> -->
         </div>
-        <TaskActions @edit="isEdit = true" v-show="!isEdit"/>
+        <TaskActions @edit="isEdit = true" v-show="!isEdit" />
     </li>
 </template>
 
 <script setup>
 import { computed, ref } from "vue";
-import TaskActions from "./TaskActions.vue"
-
+import TaskActions from "./TaskActions.vue";
 
 const props = defineProps({
     task: Object
 })
+
+const emit = defineEmits(['updated'])
 
 const isEdit = ref(false)
 const completedClass = computed(() => props.task.is_completed ? "completed" : "")
@@ -34,4 +39,9 @@ const vFocus = {
     mounted: (el) => el.focus()
 }
 
+const updateTask = event => {
+    const updatedTask = { ...props.task, name: event.target.value }
+    isEdit.value = false
+    emit('updated', updatedTask)
+}
 </script>
--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -5,30 +5,31 @@
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
                     <NewTask @added="handleAddedTask" />
+
                     <!-- List of uncompleted tasks -->
-                    <Tasks :tasks="uncompletedTasks" />
-                    
+                    <Tasks :tasks="uncompletedTasks" @updated="handleUpdatedTask" />
+
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
-                        <button class="btn btn-sm btn-secondary"
-                            @click="$event => showCompletedTasks = !showCompletedTasks">
-                            <span v-if="!showCompletedTasks">Show Completed</span>
-                            <span v-else>Hide Completed</span>
+                        <button class="btn btn-sm btn-secondary" 
+                            @click="showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
+                            <span v-else>Hide completed</span>
                         </button>
                     </div>
+
                     <!-- list of completed tasks -->
                     <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks" />
-
                 </div>
             </div>
         </div>
     </main>
 </template>
 <script setup>
-import{ computed, onMounted, ref } from "vue"
-import { allTasks, createTask } from "../http/task-api"
-import Tasks from "../components/tasks/Tasks.vue"
-import NewTask from "../components/tasks/NewTask.vue"
+import { computed, onMounted, ref } from "vue";
+import { allTasks, createTask, updateTask } from "../http/task-api";
+import Tasks from "../components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
 
 const tasks = ref([])
 
@@ -50,5 +51,13 @@ const showCompletedTasks = ref(false)
 const handleAddedTask = async (newTask) => {
     const { data: createdTask } = await createTask(newTask)
     tasks.value.unshift(createdTask.data)
+}
+
+const handleUpdatedTask = async (task) => {
+    const { data: updatedTask } = await updateTask(task.id, {
+        name: task.name
+    })
+    const currentTask = tasks.value.find(item => item.id === task.id)
+    currentTask.name = updatedTask.data.name
 }
 </script>
