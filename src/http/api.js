f4aa1afe2722342f60204c7f0ddf5944724f6002
catstring <146455510+catstring@users.noreply.github.com>
2024-04-07 22:07:04 +0800

    Update api.js

--- src/http/api.js
+++ src/http/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
     const api = axios.create({
-    baseURL: "http://todo-app-back-laravel.test/api/v1"
+    baseURL: "http://todo-backend-laravel.railway.internal/api/v1"
     });
 export default api;
