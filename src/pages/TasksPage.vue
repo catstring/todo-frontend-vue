7a98135c3574292cdbd30db57d4e1208d92cab6c
Todd MC Tsai <toddmctsai@gmail.com>
2024-04-25 17:24:44 +0800

    install pinia, define stores, getters

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -38,10 +38,21 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useTaskStore } from "../stores/task"
 import { allTasks, createTask, updateTask, completeTask, removeTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
 
+const store = useTaskStore()
+const { completedTasks, uncompletedTasks } = storeToRefs(store)
+
+// store.$patch({
+//     task: {
+//         name: "First task updated using $patch",
+//         is_completed: true
+//     }
+// })
 const tasks = ref([])
 
 onMounted(async () => {
@@ -49,9 +60,6 @@ onMounted(async () => {
     tasks.value = data.data
 })
 
-const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
-
-const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
 
 const showToggleCompletedBtn = computed(
     () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
