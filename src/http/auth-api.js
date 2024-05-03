6eb9eb00404e43f0d3393f42e981355abb2f4100
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-03 17:20:24 +0800

    add logout, dropdown button logic

--- src/http/auth-api.js
+++ src/http/auth-api.js
@@ -10,18 +10,18 @@ export const csrfCookie = () => api.get('/sanctum/csrf-cookie')
 export const login = async (credentials) => {
     try {
         const response = await api.post('/auth/login', credentials);
-        // Store the token in localStorage or sessionStorage
         localStorage.setItem('authToken', response.data.token);
-        // console.log(response.data.token)
-        return response.data;
     } catch (error) {
-        // Handle error appropriately
         throw error;
     }
 };
 
 export const register = (user) => api.post('/auth/register', user)
 
-export const logout = () => api.post('/auth/logout')
+// export const logout = () => api.post('/auth/logout')
+export const logout = () => {
+    localStorage.removeItem('authToken'); // or sessionStorage
+    api.post('/auth/logout');
+}
 
 export const getUser = () => api.get('/api/user')
