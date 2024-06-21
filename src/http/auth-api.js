65641f747bd549429977f31192bc3c1b20170bcf
Todd MC Tsai <toddmctsai@gmail.com>
2024-06-21 20:28:30 +0800

    add .env variables

--- src/http/auth-api.js
+++ src/http/auth-api.js
@@ -1,8 +1,8 @@
 import api from "./api"
 
-const baseURL = "/toddo"
+const subpathPrefix = import.meta.env.VITE_SUBPATH_PREFIX
 
-export const csrfCookie = () => api.get(`${baseURL}/sanctum/csrf-cookie`)
+export const csrfCookie = () => api.get(`${subpathPrefix}/sanctum/csrf-cookie`)
 
 // export const login = async (credentials) => {
 //         const response = await api.post('/auth/login', credentials);
@@ -11,19 +11,19 @@ export const csrfCookie = () => api.get(`${baseURL}/sanctum/csrf-cookie`)
 
 export const login = async (credentials) => {
     try {
-        const response = await api.post(`${baseURL}/auth/login`, credentials);
+        const response = await api.post(`${subpathPrefix}/auth/login`, credentials);
         localStorage.setItem('authToken', response.data.token);
     } catch (error) {
         throw error;
     }
 };
 
-export const register = (user) => api.post(`${baseURL}/auth/register`, user)
+export const register = (user) => api.post(`${subpathPrefix}/auth/register`, user)
 
 // export const logout = () => api.post('/auth/logout')
 export const logout = () => {
     localStorage.removeItem('authToken'); // or sessionStorage
-    api.post(`${baseURL}/auth/logout`);
+    api.post(`${subpathPrefix}/auth/logout`);
 }
 
-export const getUser = () => api.get(`${baseURL}/api/user`)
+export const getUser = () => api.get(`${subpathPrefix}/api/user`)
